e24101b61b6a78c60f24f2b19c517e01be0243fb
FaustGit93 <164161226+FaustGit93@users.noreply.github.com>
2026-06-20 21:25:28 +0200

    debugging....

--- admin.php
+++ admin.php
@@ -1,10 +1,12 @@
 <?php
 
+
+
+session_start();
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-session_start();
 
 if (!isset($_SESSION['admin_loggato']) || $_SESSION['admin_loggato'] !== true) {
     header("Location: login.php");
