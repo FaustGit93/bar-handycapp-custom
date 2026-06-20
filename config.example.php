df5702f6507ff69663c0910342e1cd514e9f801b
FaustGit93 <164161226+FaustGit93@users.noreply.github.com>
2026-06-20 20:11:22 +0200

    w/ on hide logic

--- config.example.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Esempio di configurazione delle credenziali.
-// Rinomina questo file in 'config.php' e imposta la tua password.
-define('ADMIN_PASSWORD', 'INSERISCI_QUI_LA_PASSWORD_SICURA');
-?>
