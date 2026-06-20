697534dddd7bfe507daa9edad3c9518cf471e702
FaustGit93 <164161226+FaustGit93@users.noreply.github.com>
2026-06-20 18:36:32 +0200

    Hide logic done -- W/ on  allergenic

--- login.php
+++ login.php
@@ -2,6 +2,11 @@
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 session_start();
 
 include 'config.php';
