4c27331670ea33d9711dcc0406e12ae35bef6f19
FaustGit93 <164161226+FaustGit93@users.noreply.github.com>
2026-06-22 20:55:22 +0200

    W/ security

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Admin - Bar HandyCapp</title>
+    <title>Login Admin - Bar</title>
     <link rel="stylesheet" href="style/login.css">
 </head>
 <body>
