f8966f6e1f7917db6cace770ac474b170a080ce6
FaustGit93 <164161226+FaustGit93@users.noreply.github.com>
2026-07-05 18:31:18 +0200

    w/ on call for translate

--- menu.php
+++ menu.php
@@ -125,11 +125,11 @@ $categorie_query = $conn->query("SELECT * FROM categorie WHERE visibile = 1 ORDE
     <div class="lang-fab">
         <input type="checkbox" id="lang-toggle-menu" class="lang-toggle-input">
         <label for="lang-toggle-menu" class="lang-selected-menu">
-            <img src="https://flagcdn.com/h40/<?php echo $lang === 'it' ? 'it' : ($lang === 'pt' ? 'br' : 'gb'); ?>.png" width="24" height="18">
+            <img src="https://flagcdn.com/w40/<?php echo $lang === 'it' ? 'it' : ($lang === 'pt' ? 'br' : 'gb'); ?>.png" width="24" height="18">
         </label>
         <div class="lang-options-menu">
             <a href="?lang=it" class="<?php echo $lang === 'it' ? 'attiva' : ''; ?>">
-                <img src="https://flagcdn.com/h40/it.png" width="20" height="15"> IT
+                <img src="https://flagcdn.com/w40/it.png" width="20" height="15"> IT
             </a>
             <a href="?lang=en" class="<?php echo $lang === 'en' ? 'attiva' : ''; ?>">
                 <img src="https://flagcdn.com/w40/gb.png" width="20" height="15"> EN
